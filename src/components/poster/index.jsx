0c6f7e0b640e523125997669c297366bd2af600c
liaofangzhengqing <liaofangzhengqing1@gmail.com>
2024-08-02 23:04:09 +0800

    feat: 样式

--- src/components/poster/index.jsx
+++ src/components/poster/index.jsx
@@ -3,6 +3,7 @@ import { Image, Space } from 'antd-mobile'
 import peoplePng from '../../assets/images/people.png';
 import book1Png from '../../assets/images/book1.png';
 import book2Png from '../../assets/images/book2.png';
+import { INFO_BOX_IMAGE } from '../../assets/images/base64-image'
 import './index.css'
 
 
@@ -13,29 +14,37 @@ export const Poster = () => {
                 <div style={{
                     height: '400px'
                 }}>
-                    浅予深深托管班开班了
+                    <div className='css3fb8b2440f25da0'>浅予深深托管班开班了</div>
                     <div style={{
                         position: "relative",
+                        marginTop: '20px',
                     }}>
                         <div style={{
                             position: 'absolute',
-                            top: '80px',
-                            left: '130px'
+                            top: '70px',
+                            left: '120px'
                         }}>
                             <Image src={book1Png} width={80} height={80} />
                         </div>
                         <div style={{
                             position: 'absolute',
-                            top: '100px',
-                            left: '160px'
+                            top: '90px',
+                            left: '150px'
                         }}>
                             <Image src={book2Png} width={80} height={80} />
                         </div>
                         <div style={{
                             position: 'absolute',
-                            // left: '130px'
                         }}>
                             <Image src={peoplePng} width={180} height={300} />
+                        </div>
+                        <div style={{
+                            position: 'absolute',
+                            top: '130px',
+                            left: '50%',
+                            transform: 'translateX(-50%)',
+                        }}>
+                            <Image src={INFO_BOX_IMAGE} width={360} height={500}/>
                         </div>
                     </div>
                 </div>
